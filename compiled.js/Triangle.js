e56ec39a170020816f77ff293f53669a622367e3
Yaniv Leviathan <leviathan@leviathan-macbookpro2.roam.corp.google.com>
2018-01-02 13:28:48 +0100

    .

--- compiled.js/Triangle.js
+++ compiled.js/Triangle.js
@@ -20,6 +20,7 @@ export default class Triangle {
     }
     draw(context) {
         context.beginPath();
+        // TODO: refactor this into a Renderer class.
         let v1 = this.p2.pos.sub(this.p1.pos);
         let v2 = this.p3.pos.sub(this.p1.pos);
         let normal = v1.cross(v2);
@@ -27,17 +28,7 @@ export default class Triangle {
         let light_vec = light_source.sub(this.center);
         light_vec.normalize();
         let cos_angle = normal.dot(light_vec);
-        // cos_angle *= 255;
-        // if (cos_angle < 0) {
-        //   cos_angle = 0;
-        // }
-        // cos_angle += 50;
-        // if (cos_angle > 255) {
-        //   cos_angle = 255;
-        // }
-        // context.fillStyle = 'rgb(' + (cos_angle | 0) + ', 0, 0)';
-        // context.fillStyle = this.color.toString();
-        context.fillStyle = this.color.multiply(cos_angle * UIValue('light_strength', 3, 0, 10, 1), 0.5);
+        context.fillStyle = this.color.multiply(cos_angle * UIValue('diffuse_light', 3, 0, 10, 1), UIValue('ambient_light', 0.5, 0, 1, 0.1));
         context.moveTo(this.p1.pos.x | 0, this.p1.pos.y | 0);
         context.lineTo(this.p2.pos.x | 0, this.p2.pos.y | 0);
         context.lineTo(this.p3.pos.x | 0, this.p3.pos.y | 0);
